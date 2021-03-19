f35e63f86f28da08b89caf0611a2705b76357393
Replit user <replituser@example.com>
2021-03-19 17:14:18 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -125,7 +125,7 @@ Función que suma los vectores `a` y `b` y
 actualiza el vector `c` con el resultado de la suma.
 */
 void sumaV(Vector * a, Vector * b, Vector * c) {
-   
+   printf("la a vale %d",a->capacidad);
    
 }
 
